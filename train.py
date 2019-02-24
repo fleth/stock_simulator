93884fd0a525925b6a9a8204458d69684ab51aac
fleth <fleth@users.noreply.github.com>
2019-02-24 20:22:15 +0900

    update trainer

--- train.py
+++ train.py
@@ -24,6 +24,7 @@ import collections
 
 sys.path.append("lib")
 import utils
+import keras_utils
 import strategy
 from loader import Loader
 from autoencoder import Autoencoder
@@ -43,19 +44,48 @@ end_date = utils.format(args[3])
 def load(setting, start_date, end_date):
     # データ準備
     X, Y = [], []
-    x_pattern = ["open", "gradient", "rsi", "rci"]
-    y_pattern = ["close"]
+
+    x_columns = None
+    x_columns = ["daily_gradient", "weekly_gradient"]
+
     stocks = Loader.stocks()
-    stock_data = Loader.loads(stocks["code"], start_date, end_date)
-    strategy_setting = strategy.StrategySetting()
-    for data in stock_data:
-        data = strategy.add_stats(data, strategy_setting)
-        X.extend(data[x_pattern].as_matrix())
-        Y.extend(data[y_pattern].as_matrix())
+    stock_data = Loader.loads(stocks["code"], start_date, end_date, with_stats=True)
+    for code, data in stock_data.items():
+        x = data.drop("date", axis=1)
+        y = data["entity"] * data["yin"] # 実体と方向
+        shape = setting.batch_input_shape if setting.with_batch else setting.input_shape
+
+        if x_columns is not None:
+            x = x[x_columns]
+
+        if setting.with_batch:
+            x = to_seq(setting, x)
+            x = numpy.nan_to_num(x.reshape(shape))
+            y = y.iloc[setting.length_of_sequences:]
+        else:
+            x = numpy.nan_to_num(x.as_matrix().reshape(shape))
+        y = numpy.nan_to_num(y.as_matrix())
+        X.extend(x)
+        Y.extend(y)
     X = numpy.array(X)
     Y = numpy.array(Y)
     print(X.shape, Y.shape)
     return X, Y
+
+def to_seq(setting, data):
+    seq = []
+    length = setting.length_of_sequences
+    for i in range(length):
+        r = -(length-(i+1))
+        if r == 0:
+            d = data.iloc[i:-1]
+        else:
+            d = data.iloc[i:r-1]
+#        print(i, r, len(d))
+        seq.append(d.as_matrix())
+    seq = numpy.array(seq)
+    print(seq.shape, setting.batch_input_shape)
+    return seq
 
 # ブースティング用重みファイルのリスト
 def weights():
@@ -77,8 +107,8 @@ def output_tsv(setting):
     Y = Y.reshape(-1, 1)
     print("x_train", X.shape)
     print("y_train", Y.shape)
-    utils.output_csv(X, "x_train.tsv", sep='\t')
-    utils.output_csv(Y, "y_train.tsv", sep='\t')
+    keras_utils.output_csv(X, "x_train.tsv", sep='\t')
+    keras_utils.output_csv(Y, "y_train.tsv", sep='\t')
 
     path = "simulator/results"
     if not os.path.exists(path):
@@ -86,7 +116,7 @@ def output_tsv(setting):
 
     for i, x in enumerate(X):
         print(x)
-        utils.output_pgm(x, "%s/x_%s.pgm" % (path, i), 100, 5)
+        keras_utils.output_pgm(x, "%s/x_%s.pgm" % (path, i), 100, 5)
 
 def autoencode(setting):
     setting.hidden_activation = "relu"
@@ -96,7 +126,7 @@ def autoencode(setting):
     autoencoder = Autoencoder(setting)
 
     X, Y = load(setting)
-    utils.output_csv(X, "strategy.csv")
+    keras_utils.output_csv(X, "strategy.csv")
     split_pos = int(len(X) * setting.split)
     x = X[0:split_pos]
     x_test = X[split_pos:]
@@ -107,7 +137,7 @@ def autoencode(setting):
 
 def svm(setting):
     X, Y = load(setting)
-    x, y, x_test, y_test = utils.validate_split(X, Y, setting.split)
+    x, y, x_test, y_test = keras_utils.validate_split(X, Y, setting.split)
 
     model = sklearn.svm.SVC()
     model.fit(x, y)
@@ -116,7 +146,7 @@ def svm(setting):
 
 def auto_sk(setting):
     X, Y = load(setting)
-    x, y, x_test, y_test = utils.validate_split(X, Y, setting.split)
+    x, y, x_test, y_test = keras_utils.validate_split(X, Y, setting.split)
 
     automl = AutoSklearnClassifier()
     automl.fit(x, y)
@@ -127,7 +157,7 @@ def auto_sk(setting):
 
 def train(setting, model, start_date, end_date, output=True):
     X, Y = load(setting, start_date, end_date)
-    x, y, x_test, y_test = utils.validate_split(X, Y, setting.split)
+    x, y, x_test, y_test = keras_utils.validate_split(X, Y, setting.split)
 
     print(x.shape, y.shape, x_test.shape, y_test.shape)
 
@@ -140,7 +170,7 @@ def train(setting, model, start_date, end_date, output=True):
 
 def boost(setting, model, output=True):
     X, Y = load(setting)
-    x, y, x_test, y_test = utils.under_sampling(X, Y, setting.split)
+    x, y, x_test, y_test = keras_utils.under_sampling(X, Y, setting.split)
 
     trainer = Trainer(setting, load_model)
     predict = trainer.predict(x)
@@ -164,8 +194,8 @@ def bagging(setting, model):
     print("chunk: %s" % len(chunked_x))
 
     for x, y in zip(chunked_x, chunked_y):
-#        x, y, x_test, y_test = utils.under_sampling(x, y, setting.split)
-        x, y, x_test, y_test = utils.validate_split(x, y, setting.split)
+#        x, y, x_test, y_test = keras_utils.under_sampling(x, y, setting.split)
+        x, y, x_test, y_test = keras_utils.validate_split(x, y, setting.split)
         trainer = Trainer(setting, model)
         trainer.train(x, y, x_test, y_test)
         eval_accuracy(trainer, x_test, y_test, output=False)
@@ -174,7 +204,7 @@ def bagging(setting, model):
 
 def validate(setting, model):
     X, Y = load(setting)
-    x, y, x_test, y_test = utils.under_sampling(X, Y, 0.5)
+    x, y, x_test, y_test = keras_utils.under_sampling(X, Y, 0.5)
 
     trainer = Trainer(setting, load_model)
     return eval_accuracy(trainer, x_test, y_test)
@@ -184,7 +214,7 @@ def validate_boost(setting, model):
     files = weights()
 
     X, Y = load(setting)
-    x, y, x_test, y_test = utils.under_sampling(X, Y, 0.5)
+    x, y, x_test, y_test = keras_utils.under_sampling(X, Y, 0.5)
 
     trainer = Trainer(setting, load_model)
 
@@ -217,8 +247,8 @@ def eval_accuracy(trainer, x, y, timestamp=None, output=True):
     if not output:
         return loss, acc, re, pre
 
-    utils.output_csv(predict, "predict.csv", timestamp)
-    utils.output_csv(y, "y_test.csv")
+    keras_utils.output_csv(predict, "predict.csv", timestamp)
+    keras_utils.output_csv(y, "y_test.csv")
 
     return loss, acc, re, pre
 
@@ -245,26 +275,27 @@ def correct(y, predict):
 if __name__ == "__main__":
   setting = TrainerSetting()
   setting.split = 0.8
-  setting.input_neurons = 3
-  setting.input_shape = (-1, setting.input_neurons)
-  setting.batch_input_shape = (-1, setting.input_neurons, 1)
-  setting.output_shape = (-1)
+  setting.input_neurons = 2
+  setting.length_of_sequences = 5
   setting.output_neurons = 1
-  setting.hidden_neurons = 512
+  setting.hidden_neurons = 128
   setting.filter_size = 128
   setting.batch_size = 500
   setting.nb_epoch = 500
-  setting.depth = 2
+  setting.depth = 0
   setting.dropout = 0.02
   setting.hidden_activation = "relu"
   setting.output_activation = "linear"
   setting.loss = "mean_squared_error"
   setting.optimizers = "adam"
   setting.metrics = ["accuracy"]
+  setting.input_shape = (-1, setting.input_neurons)
+  setting.batch_input_shape = (-1, setting.length_of_sequences, setting.input_neurons)
+  setting.output_shape = (-1)
 
   setting.categorical = False
-  setting.with_batch = False
-  model = ModelCreator.dnn
+  setting.with_batch = True
+  model = ModelCreator.lstm
 
   if mode == "train":
     train(setting, model, start_date, end_date)
