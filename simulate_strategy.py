2fa4b1dd2d3b34d9f6e025bf8862343eb3aa424c
fleth <fleth@users.noreply.github.com>
2020-09-21 20:47:57 +0900

    add min_assets

--- simulate_strategy.py
+++ simulate_strategy.py
@@ -322,7 +322,7 @@ def create_performance(args, simulator_setting, performances):
 
     # 簡易レポート
     for date, performance in sorted(performances.items(), key=lambda x: utils.to_datetime(x[0])):
-        pickup = ["gain", "trade", "win_trade", "max_drawdown", "max_unavailable_assets", "auto_stop_loss"]
+        pickup = ["gain", "min_assets", "trade", "win_trade", "max_drawdown", "max_unavailable_assets", "auto_stop_loss"]
         stats = list(map(lambda x: "%s: %s" % (x, performance[x]), pickup))
         print(date, ",\t".join(stats))
 
